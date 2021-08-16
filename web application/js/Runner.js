777f2340c30acbeb731a336d84b671d633b08cf2
肖子立 <xiaozlb@digitalchina.com>
2021-08-16 17:45:35 +0800

    running image

--- web application/js/Runner.js
+++ web application/js/Runner.js
@@ -2,6 +2,7 @@ class Runner {
   constructor() {
     this.x = 0; // 已跑步数
     this.y = 3; // 所在的跑道号
+    this.frame = 0; // 当前帧
   }
 
   running() {
@@ -9,15 +10,17 @@ class Runner {
       "imgs/runner_1.png",
       "imgs/runner_2.png",
       "imgs/runner_3.png",
-      "imgs/runner_4.png",
+      "imgs/runner_4.png"
     ];
-    const runner = $("#runner");
-    const img = $("#runner img");
-
     // 跑步动画
+    this.timer = setInterval(() => {
+      gsap.set("#runner img", { attr: { src: runnerSrcs[this.frame % 3] } });
+      this.frame++;
+    }, ONE_FRAME * 5);
   }
 
   stop() {
+    if (this.timer) clearInterval(this.timer);
   }
 
   changeRunWays() {}
